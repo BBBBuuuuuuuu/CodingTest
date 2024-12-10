f81001e8bbc7a972be55be329a3d01d8997bbc24
BBBBuuuuuuuu <160101827+BBBBuuuuuuuu@users.noreply.github.com>
2024-12-10 16:49:26 +0900

    Add files via upload

---
+++ Code/star2438.py
@@ -0,0 +1,15 @@
+count = int(input())
+
+for i in range(1, count+1):
+    for j in range(1, i+1):
+        print("*", end="")
+    if i != count:
+        print()
+
+
+
+# n=int(input())
+# for i in range(n):
+#     i+=1
+#     print("*"*i)
+
